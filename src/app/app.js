2e819c89e6e1e0ae9e1c0121725ea926c369bc10
Andre Perez <andreperezgarza@hotmail.com>
2022-11-17 20:05:07 -0600

    updated code

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,12 @@
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello Yorch\n')
-  })
+const hostname = "localhost://";
+const port = "800";
 
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
-  })
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello Team\n");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
